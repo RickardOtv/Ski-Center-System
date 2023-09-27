fef49716d50f2ae64aa4febd9fe29e6fb17c1aa7
Max <s2106116@student.hb.se>
2023-09-27 11:56:52 +0200

    gui jobb

--- NetFramework/NetFramework/LoggaIn.Designer.cs
+++ NetFramework/NetFramework/LoggaIn.Designer.cs
@@ -28,21 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAvbryt = new System.Windows.Forms.Button();
+            this.btnLoggaIn = new System.Windows.Forms.Button();
+            this.txtLösen = new System.Windows.Forms.TextBox();
+            this.txtAnstNr = new System.Windows.Forms.TextBox();
+            this.lblLösenord = new System.Windows.Forms.Label();
+            this.lblAnstNr = new System.Windows.Forms.Label();
+            this.lblLoggaIn = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnAvbryt
+            // 
+            this.btnAvbryt.Location = new System.Drawing.Point(342, 244);
+            this.btnAvbryt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAvbryt.Name = "btnAvbryt";
+            this.btnAvbryt.Size = new System.Drawing.Size(81, 29);
+            this.btnAvbryt.TabIndex = 13;
+            this.btnAvbryt.Text = "Avbryt";
+            this.btnAvbryt.UseVisualStyleBackColor = true;
+            this.btnAvbryt.Click += new System.EventHandler(this.btnAvbryt_Click);
+            // 
+            // btnLoggaIn
+            // 
+            this.btnLoggaIn.Location = new System.Drawing.Point(153, 244);
+            this.btnLoggaIn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLoggaIn.Name = "btnLoggaIn";
+            this.btnLoggaIn.Size = new System.Drawing.Size(84, 29);
+            this.btnLoggaIn.TabIndex = 12;
+            this.btnLoggaIn.Text = "Logga In";
+            this.btnLoggaIn.UseVisualStyleBackColor = true;
+            this.btnLoggaIn.Click += new System.EventHandler(this.btnLoggaIn_Click);
+            // 
+            // txtLösen
+            // 
+            this.txtLösen.Location = new System.Drawing.Point(301, 133);
+            this.txtLösen.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLösen.Name = "txtLösen";
+            this.txtLösen.Size = new System.Drawing.Size(145, 26);
+            this.txtLösen.TabIndex = 11;
+            // 
+            // txtAnstNr
+            // 
+            this.txtAnstNr.Location = new System.Drawing.Point(301, 102);
+            this.txtAnstNr.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAnstNr.Name = "txtAnstNr";
+            this.txtAnstNr.Size = new System.Drawing.Size(145, 26);
+            this.txtAnstNr.TabIndex = 10;
+            // 
+            // lblLösenord
+            // 
+            this.lblLösenord.AutoSize = true;
+            this.lblLösenord.Location = new System.Drawing.Point(119, 133);
+            this.lblLösenord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLösenord.Name = "lblLösenord";
+            this.lblLösenord.Size = new System.Drawing.Size(76, 20);
+            this.lblLösenord.TabIndex = 9;
+            this.lblLösenord.Text = "Lösenord";
+            // 
+            // lblAnstNr
+            // 
+            this.lblAnstNr.AutoSize = true;
+            this.lblAnstNr.Location = new System.Drawing.Point(119, 102);
+            this.lblAnstNr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAnstNr.Name = "lblAnstNr";
+            this.lblAnstNr.Size = new System.Drawing.Size(155, 20);
+            this.lblAnstNr.TabIndex = 8;
+            this.lblAnstNr.Text = "AnställningsNummer";
+            // 
+            // lblLoggaIn
+            // 
+            this.lblLoggaIn.AutoSize = true;
+            this.lblLoggaIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoggaIn.Location = new System.Drawing.Point(232, 31);
+            this.lblLoggaIn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLoggaIn.Name = "lblLoggaIn";
+            this.lblLoggaIn.Size = new System.Drawing.Size(142, 37);
+            this.lblLoggaIn.TabIndex = 7;
+            this.lblLoggaIn.Text = "Logga In";
             // 
             // LoggaIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.btnAvbryt);
+            this.Controls.Add(this.btnLoggaIn);
+            this.Controls.Add(this.txtLösen);
+            this.Controls.Add(this.txtAnstNr);
+            this.Controls.Add(this.lblLösenord);
+            this.Controls.Add(this.lblAnstNr);
+            this.Controls.Add(this.lblLoggaIn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LoggaIn";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.LoggaIn_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAvbryt;
+        private System.Windows.Forms.Button btnLoggaIn;
+        private System.Windows.Forms.TextBox txtLösen;
+        private System.Windows.Forms.TextBox txtAnstNr;
+        private System.Windows.Forms.Label lblLösenord;
+        private System.Windows.Forms.Label lblAnstNr;
+        private System.Windows.Forms.Label lblLoggaIn;
     }
 }
 
--- NetFramework/NetFramework/LoggaIn.cs
+++ NetFramework/NetFramework/LoggaIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datalager;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,45 @@ namespace NetFramework
 {
     public partial class LoggaIn : Form
     {
+
+
+        ExpeditKontroller expeditKontroller = new ExpeditKontroller();
+        UnitOfWork unitOfWork = new UnitOfWork();
         public LoggaIn()
         {
             InitializeComponent();
         }
 
-        private void LoggaIn_Load(object sender, EventArgs e)
+        //Knapp för att logga in och ta sig till huvudmenyn
+        private void btnLoggaIn_Click(object sender, EventArgs e)
         {
+            int angivetAnstNr = int.Parse(txtAnstNr.Text);
+            //LINQ-satser för att matcha anställningsnummer / lösenord med de lagrade värdena
+            var anstNr = (from exp in unitOfWork.expediter where exp.ExpeditID == angivetAnstNr select exp.ExpeditID).FirstOrDefault();
+            var lösen = (from exp in unitOfWork.expediter where exp.lösenord == txtLösen.Text select exp.lösenord).FirstOrDefault();
 
+            bool inmatning = expeditKontroller.LoggaIn(anstNr, lösen);
+            if (inmatning == true)
+            {
+                HuvudMeny huvudMeny = new HuvudMeny(this, expeditKontroller);
+                huvudMeny.Show();
+            }
+            else
+                MessageBox.Show("Inloggning misslyckades");
         }
+
+        //Avsluta programmet
+        private void btnAvbryt_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        
+
+        
+
+       
+
+        
     }
 }
