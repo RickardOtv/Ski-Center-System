2cc4861659e51680edd0ea541a72e2cc631a6a96
S2108656@student.hb.se <S2108656@student.hb.se>
2023-09-29 13:13:40 +0200

    New design inlog

--- NetFramework/NetFramework/LoggaIn.Designer.cs
+++ NetFramework/NetFramework/LoggaIn.Designer.cs
@@ -39,78 +39,82 @@
             // 
             // btnAvbryt
             // 
-            this.btnAvbryt.Location = new System.Drawing.Point(342, 244);
-            this.btnAvbryt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAvbryt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAvbryt.Location = new System.Drawing.Point(247, 348);
+            this.btnAvbryt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAvbryt.Name = "btnAvbryt";
-            this.btnAvbryt.Size = new System.Drawing.Size(81, 54);
+            this.btnAvbryt.Size = new System.Drawing.Size(132, 68);
             this.btnAvbryt.TabIndex = 13;
             this.btnAvbryt.Text = "Avbryt";
-            this.btnAvbryt.UseVisualStyleBackColor = true;
+            this.btnAvbryt.UseVisualStyleBackColor = false;
             this.btnAvbryt.Click += new System.EventHandler(this.btnAvbryt_Click);
             // 
             // btnLoggaIn
             // 
-            this.btnLoggaIn.Location = new System.Drawing.Point(153, 244);
-            this.btnLoggaIn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLoggaIn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLoggaIn.Location = new System.Drawing.Point(442, 348);
+            this.btnLoggaIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLoggaIn.Name = "btnLoggaIn";
-            this.btnLoggaIn.Size = new System.Drawing.Size(99, 54);
+            this.btnLoggaIn.Size = new System.Drawing.Size(132, 68);
             this.btnLoggaIn.TabIndex = 12;
             this.btnLoggaIn.Text = "Logga In";
-            this.btnLoggaIn.UseVisualStyleBackColor = true;
+            this.btnLoggaIn.UseVisualStyleBackColor = false;
             this.btnLoggaIn.Click += new System.EventHandler(this.btnLoggaIn_Click);
             // 
             // txtLösen
             // 
-            this.txtLösen.Location = new System.Drawing.Point(301, 133);
-            this.txtLösen.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLösen.Location = new System.Drawing.Point(321, 274);
+            this.txtLösen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtLösen.Name = "txtLösen";
-            this.txtLösen.Size = new System.Drawing.Size(145, 26);
+            this.txtLösen.Size = new System.Drawing.Size(192, 31);
             this.txtLösen.TabIndex = 11;
             // 
             // txtAnstNr
             // 
-            this.txtAnstNr.Location = new System.Drawing.Point(301, 102);
-            this.txtAnstNr.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAnstNr.Location = new System.Drawing.Point(321, 209);
+            this.txtAnstNr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtAnstNr.Name = "txtAnstNr";
-            this.txtAnstNr.Size = new System.Drawing.Size(145, 26);
+            this.txtAnstNr.Size = new System.Drawing.Size(192, 31);
             this.txtAnstNr.TabIndex = 10;
             // 
             // lblLösenord
             // 
             this.lblLösenord.AutoSize = true;
-            this.lblLösenord.Location = new System.Drawing.Point(119, 133);
-            this.lblLösenord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLösenord.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLösenord.Location = new System.Drawing.Point(213, 277);
             this.lblLösenord.Name = "lblLösenord";
-            this.lblLösenord.Size = new System.Drawing.Size(76, 20);
+            this.lblLösenord.Size = new System.Drawing.Size(102, 25);
             this.lblLösenord.TabIndex = 9;
             this.lblLösenord.Text = "Lösenord";
             // 
             // lblAnstNr
             // 
             this.lblAnstNr.AutoSize = true;
-            this.lblAnstNr.Location = new System.Drawing.Point(119, 102);
-            this.lblAnstNr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAnstNr.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAnstNr.Location = new System.Drawing.Point(106, 212);
             this.lblAnstNr.Name = "lblAnstNr";
-            this.lblAnstNr.Size = new System.Drawing.Size(155, 20);
+            this.lblAnstNr.Size = new System.Drawing.Size(209, 25);
             this.lblAnstNr.TabIndex = 8;
             this.lblAnstNr.Text = "AnställningsNummer";
             // 
             // lblLoggaIn
             // 
             this.lblLoggaIn.AutoSize = true;
+            this.lblLoggaIn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblLoggaIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLoggaIn.Location = new System.Drawing.Point(165, 31);
-            this.lblLoggaIn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLoggaIn.Location = new System.Drawing.Point(121, 9);
             this.lblLoggaIn.Name = "lblLoggaIn";
-            this.lblLoggaIn.Size = new System.Drawing.Size(212, 55);
+            this.lblLoggaIn.Size = new System.Drawing.Size(562, 73);
             this.lblLoggaIn.TabIndex = 7;
-            this.lblLoggaIn.Text = "Logga In";
+            this.lblLoggaIn.Text = "Ski Center System";
             // 
             // LoggaIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.BackgroundImage = global::NetFramework.Properties.Resources._1286ce2aae8bf42293dc770d573c0eb0;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnAvbryt);
             this.Controls.Add(this.btnLoggaIn);
             this.Controls.Add(this.txtLösen);
@@ -118,7 +122,7 @@
             this.Controls.Add(this.lblLösenord);
             this.Controls.Add(this.lblAnstNr);
             this.Controls.Add(this.lblLoggaIn);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "LoggaIn";
             this.Text = "Form1";
             this.ResumeLayout(false);
