6a17458a81999d197c1087b545d238b3f8265f54
Rickard Otvos <s2106051@student.hb.se>
2023-10-04 15:39:13 +0200

    fixa kundregister grid

--- NetFramework/NetFramework/Kundregister.Designer.cs
+++ NetFramework/NetFramework/Kundregister.Designer.cs
@@ -47,20 +47,18 @@
             // 
             // txtAnvandarnamn
             // 
-            this.txtAnvandarnamn.Location = new System.Drawing.Point(168, 7);
-            this.txtAnvandarnamn.Margin = new System.Windows.Forms.Padding(5);
+            this.txtAnvandarnamn.Location = new System.Drawing.Point(112, 5);
             this.txtAnvandarnamn.Name = "txtAnvandarnamn";
-            this.txtAnvandarnamn.Size = new System.Drawing.Size(19, 26);
+            this.txtAnvandarnamn.Size = new System.Drawing.Size(14, 20);
             this.txtAnvandarnamn.TabIndex = 9;
             // 
             // lblAnstNr
             // 
             this.lblAnstNr.AutoSize = true;
             this.lblAnstNr.BackColor = System.Drawing.Color.Transparent;
-            this.lblAnstNr.Location = new System.Drawing.Point(14, 9);
-            this.lblAnstNr.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblAnstNr.Location = new System.Drawing.Point(9, 6);
             this.lblAnstNr.Name = "lblAnstNr";
-            this.lblAnstNr.Size = new System.Drawing.Size(157, 20);
+            this.lblAnstNr.Size = new System.Drawing.Size(103, 13);
             this.lblAnstNr.TabIndex = 8;
             this.lblAnstNr.Text = "Anställningsnummer:";
             // 
@@ -69,8 +67,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(436, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(291, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(210, 39);
             this.label1.TabIndex = 10;
@@ -78,9 +75,10 @@
             // 
             // btnTaBort
             // 
-            this.btnTaBort.Location = new System.Drawing.Point(989, 557);
+            this.btnTaBort.Location = new System.Drawing.Point(641, 362);
+            this.btnTaBort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTaBort.Name = "btnTaBort";
-            this.btnTaBort.Size = new System.Drawing.Size(95, 38);
+            this.btnTaBort.Size = new System.Drawing.Size(63, 25);
             this.btnTaBort.TabIndex = 11;
             this.btnTaBort.Text = "Ta bort";
             this.btnTaBort.UseVisualStyleBackColor = true;
@@ -88,9 +86,10 @@
             // 
             // btnAndra
             // 
-            this.btnAndra.Location = new System.Drawing.Point(864, 557);
+            this.btnAndra.Location = new System.Drawing.Point(563, 362);
+            this.btnAndra.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAndra.Name = "btnAndra";
-            this.btnAndra.Size = new System.Drawing.Size(95, 38);
+            this.btnAndra.Size = new System.Drawing.Size(63, 25);
             this.btnAndra.TabIndex = 12;
             this.btnAndra.Text = "Ändra";
             this.btnAndra.UseVisualStyleBackColor = true;
@@ -98,9 +97,10 @@
             // 
             // btnTillbaka
             // 
-            this.btnTillbaka.Location = new System.Drawing.Point(18, 637);
+            this.btnTillbaka.Location = new System.Drawing.Point(12, 414);
+            this.btnTillbaka.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTillbaka.Name = "btnTillbaka";
-            this.btnTillbaka.Size = new System.Drawing.Size(95, 38);
+            this.btnTillbaka.Size = new System.Drawing.Size(63, 25);
             this.btnTillbaka.TabIndex = 13;
             this.btnTillbaka.Text = "Tillbaka";
             this.btnTillbaka.UseVisualStyleBackColor = true;
@@ -109,17 +109,23 @@
             // gridKunder
             // 
             this.gridKunder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridKunder.Location = new System.Drawing.Point(132, 201);
+            this.gridKunder.Location = new System.Drawing.Point(88, 131);
+            this.gridKunder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridKunder.MultiSelect = false;
             this.gridKunder.Name = "gridKunder";
+            this.gridKunder.ReadOnly = true;
+            this.gridKunder.RowHeadersVisible = false;
             this.gridKunder.RowTemplate.Height = 28;
-            this.gridKunder.Size = new System.Drawing.Size(952, 337);
+            this.gridKunder.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridKunder.Size = new System.Drawing.Size(616, 219);
             this.gridKunder.TabIndex = 14;
             // 
             // btnSokPersonNr
             // 
-            this.btnSokPersonNr.Location = new System.Drawing.Point(393, 153);
+            this.btnSokPersonNr.Location = new System.Drawing.Point(262, 99);
+            this.btnSokPersonNr.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSokPersonNr.Name = "btnSokPersonNr";
-            this.btnSokPersonNr.Size = new System.Drawing.Size(92, 41);
+            this.btnSokPersonNr.Size = new System.Drawing.Size(61, 27);
             this.btnSokPersonNr.TabIndex = 17;
             this.btnSokPersonNr.Text = "Sök";
             this.btnSokPersonNr.UseVisualStyleBackColor = true;
@@ -129,41 +135,46 @@
             // 
             this.lblNamn.AutoSize = true;
             this.lblNamn.BackColor = System.Drawing.Color.Transparent;
-            this.lblNamn.Location = new System.Drawing.Point(128, 74);
+            this.lblNamn.Location = new System.Drawing.Point(85, 48);
+            this.lblNamn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNamn.Name = "lblNamn";
-            this.lblNamn.Size = new System.Drawing.Size(55, 20);
+            this.lblNamn.Size = new System.Drawing.Size(38, 13);
             this.lblNamn.TabIndex = 18;
             this.lblNamn.Text = "Namn:";
             // 
             // txtBoxNamn
             // 
-            this.txtBoxNamn.Location = new System.Drawing.Point(132, 97);
+            this.txtBoxNamn.Location = new System.Drawing.Point(88, 63);
+            this.txtBoxNamn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtBoxNamn.Name = "txtBoxNamn";
-            this.txtBoxNamn.Size = new System.Drawing.Size(255, 26);
+            this.txtBoxNamn.Size = new System.Drawing.Size(171, 20);
             this.txtBoxNamn.TabIndex = 19;
             // 
             // txtBoxPersonNr
             // 
-            this.txtBoxPersonNr.Location = new System.Drawing.Point(132, 160);
+            this.txtBoxPersonNr.Location = new System.Drawing.Point(88, 104);
+            this.txtBoxPersonNr.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtBoxPersonNr.Name = "txtBoxPersonNr";
-            this.txtBoxPersonNr.Size = new System.Drawing.Size(255, 26);
+            this.txtBoxPersonNr.Size = new System.Drawing.Size(171, 20);
             this.txtBoxPersonNr.TabIndex = 20;
             // 
             // lblPersonNr
             // 
             this.lblPersonNr.AutoSize = true;
             this.lblPersonNr.BackColor = System.Drawing.Color.Transparent;
-            this.lblPersonNr.Location = new System.Drawing.Point(128, 137);
+            this.lblPersonNr.Location = new System.Drawing.Point(85, 89);
+            this.lblPersonNr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPersonNr.Name = "lblPersonNr";
-            this.lblPersonNr.Size = new System.Drawing.Size(121, 20);
+            this.lblPersonNr.Size = new System.Drawing.Size(80, 13);
             this.lblPersonNr.TabIndex = 21;
             this.lblPersonNr.Text = "Personnummer:";
             // 
             // btnSokNamn
             // 
-            this.btnSokNamn.Location = new System.Drawing.Point(393, 90);
+            this.btnSokNamn.Location = new System.Drawing.Point(262, 58);
+            this.btnSokNamn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSokNamn.Name = "btnSokNamn";
-            this.btnSokNamn.Size = new System.Drawing.Size(92, 41);
+            this.btnSokNamn.Size = new System.Drawing.Size(61, 27);
             this.btnSokNamn.TabIndex = 22;
             this.btnSokNamn.Text = "Sök";
             this.btnSokNamn.UseVisualStyleBackColor = true;
@@ -173,10 +184,10 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.Location = new System.Drawing.Point(1018, 131);
+            this.button1.Location = new System.Drawing.Point(660, 88);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 63);
+            this.button1.Size = new System.Drawing.Size(44, 41);
             this.button1.TabIndex = 23;
             this.button1.Text = "↻ ";
             this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -185,11 +196,11 @@
             // 
             // Kundregister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::NetFramework.Properties.Resources.v1_txt2img_77f610f8_523e_4179_b8c4_568ee1e75053;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1143, 687);
+            this.ClientSize = new System.Drawing.Size(723, 447);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSokNamn);
             this.Controls.Add(this.lblPersonNr);
@@ -204,6 +215,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAnvandarnamn);
             this.Controls.Add(this.lblAnstNr);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Kundregister";
             this.Text = "Kundregister";
             this.Load += new System.EventHandler(this.Kundregister_Load);
