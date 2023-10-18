e541b930cfa192148079c0d7003108ee34179fcf
Sasha Stojanovic <s2106550@student.hb.se>
2023-10-18 14:52:11 +0200

    Ändrat ett litet fel, från string till int

--- NetFramework/Entitetslager/KonferensPris.cs
+++ NetFramework/Entitetslager/KonferensPris.cs
@@ -14,12 +14,12 @@ namespace Entitetslager
         [Key, Column(Order = 1)]
         public string Typ { get; set; }
         [Key, Column(Order = 2)]
-        public string Vecka { get; set; }
+        public int Vecka { get; set; }
         public int VeckoPris { get; set; }
         public int DygnsPris { get; set; }
         public int TimPris { get; set; }
 
-        public KonferensPris(string typ, string vecka, int veckoPris, int dygnsPris, int timPris)
+        public KonferensPris(string typ, int vecka, int veckoPris, int dygnsPris, int timPris)
         {
             Typ = typ;
             Vecka = vecka;
