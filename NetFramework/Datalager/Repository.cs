ab9875c77793bc9cedb39845cf65e54273bd2eb4
Rickard Otvos <ricka@RICKARDS-DATOR>
2023-10-27 14:21:17 +0200

    Datalager formaterat

--- NetFramework/Datalager/Repository.cs
+++ NetFramework/Datalager/Repository.cs
@@ -10,9 +10,9 @@ namespace Datalager
         where T : class
     {
         /// <summary>
-        ///  Lägg till en ny entitet till Table.
+        /// Lägger till en entitet i den underliggande datatabellen.
         /// </summary>
-        /// <param name="entity"></param>
+        /// <param name="entity">Entiteten som ska läggas till i datatabellen</param>
         public void Add(T entity)
         {
             table.Add(entity);
@@ -21,7 +21,7 @@ namespace Datalager
         /// <summary>
         ///  Ta bort en entitet från Table.
         /// </summary>
-        /// <param name="entity"></param>
+        /// <param name="entity">Entiteten som ska tas bort</param>
         /// <returns>true om borttagen och false annars.</returns>
         public bool Remove(T entity)
         {
@@ -29,29 +29,30 @@ namespace Datalager
         }
 
         /// <summary>
-        ///  Hitta entiteter som matchar predikatet.
+        /// Hittar och returnerar en samling av entiteter som matchar det specificerade predikatet.
         /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
+        /// <param name="predicate">Ett villkor som används för att filtrera entiteter i datatabellen</param>
+        /// <returns>En samling av entiteter som uppfyller predikatet</returns>
         public IEnumerable<T> Find(Func<T, bool> predicate)
         {
             return table.Where(predicate);
         }
 
         /// <summary>
-        ///  Hitta den första entiteten som matchar predikat.
+        /// Hittar det första elementet som matchar det specificerade villkoret och returnerar det.
+        /// Om inget matchande element hittas, returneras standardvärdet för typen T.
         /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
+        /// <param name="predicate">Ett villkor som används för att filtrera elementen i sekvensen</param>
+        /// <returns>Det första matchande elementet eller standardvärdet för typen T om ingen matchning hittas</returns>
         public T FirstOrDefault(Func<T, bool> predicate)
         {
             return table.FirstOrDefault(predicate);
         }
 
         /// <summary>
-        ///  Är repositoryn tom?
+        /// Kontrollerar om datatabellen är tom, dvs. om den inte innehåller några entiteter.
         /// </summary>
-        /// <returns>true = tom, false = inte.</returns>
+        /// <returns><c>true</c> om datatabellen är tom; annars <c>false</c>.</returns>
         public bool IsEmpty()
         {
             return table.Count == 0;
@@ -66,6 +67,7 @@ namespace Datalager
             return table.Count();
         }
 
+        // Skapar en ny instans av Repository-klassen och initierar den interna datatabellen om den inte redan finns.
         internal Repository()
         {
             if (table == null)
@@ -73,6 +75,7 @@ namespace Datalager
                 table = new List<T>();
             }
         }
+
         private IList<T> table;
     }
 }
